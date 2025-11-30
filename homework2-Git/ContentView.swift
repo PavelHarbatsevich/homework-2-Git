7022e96f805f3749548e8085441dc346c41beece
Pavel2024GP <harbatsevichpavel36@gmail.com>
2025-11-30 14:03:09 +0300

    Initial Commit

---
+++ homework2-Git/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  homework2-Git
+//
+//  Created by Павел Гарбацевич on 30.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ homework2-Git/homework2_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  homework2_GitApp.swift
+//  homework2-Git
+//
+//  Created by Павел Гарбацевич on 30.11.25.
+//
+
+import SwiftUI
+
+@main
+struct homework2_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
